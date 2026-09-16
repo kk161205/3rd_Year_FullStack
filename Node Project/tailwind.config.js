1867591ea888e4dc98074b127e929fa8e8bee3b1
kk161205 <kulshrestha.kushagrak1512@gmail.com>
2026-09-16 11:04:03 +0530

    Some new things

---
+++ Node Project/tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+      'red': {
+        windy: '#000000ff',
+        '100': '#ff0000',
+        '200': '#fff000',
+        '300': '#ffff00',
+        '400': '#fffff0',
+        '500': '#ffffff',
+        '600': '#ffffff14',
+        '700': '#ffffff0b',
+        '800': '#ffffff08',
+        '900': '#ffffff05',
+      },
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+}
